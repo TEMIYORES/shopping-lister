be4c4ee92a217f231c33b0a8befc22cfd70c1b43
temiyores <temiyores1234@gmail.com>
2023-10-12 15:45:48 +0100

    4 commit

--- index.js
+++ index.js
@@ -3,32 +3,29 @@ const inputForm = document.querySelector("form");
 const itemContainer = document.querySelector("ul");
 const clearbtn = document.querySelector("button.clearBtn");
 const filter = document.querySelector("input.filterInput");
+
+input.addEventListener("input", (e) => {
+  if (input.value.trim() === "") {
+    input.value = "";
+    return;
+  }
+});
 // add item function
 const addItem = (e) => {
   e.preventDefault();
 
   // Validate Input
   if (input.value.trim() === "") {
-    alert("Please add Item");
+    alert("Please add item");
     return;
   }
-  const item = document.createElement("li");
-  const itemTextContainer = document.createElement("p");
-  itemTextContainer.title = "tap to edit";
+  const item = itemAdd(input.value);
 
-  const itemText = document.createTextNode(`${input.value}`);
-  item.className =
-    "max-sm:w-full w-[48%] bg-white px-2 py-2 rounded-md flex items-center justify-between";
-  const icon = document.createElement("span");
-  icon.className = "material-icons text-red-700 cursor-pointer close";
-  icon.style.fontSize = "1.5rem";
-  const iconText = document.createTextNode("close");
-  icon.appendChild(iconText);
-  itemTextContainer.appendChild(itemText);
-  item.appendChild(itemTextContainer);
-  item.appendChild(icon);
   if (Array.from(itemContainer.children)[0].tagName == "SMALL") {
     itemContainer.replaceChild(item, itemContainer.children[0]);
+  }
+  if (itemContainer.lastChild.className == "filterText") {
+    itemContainer.replaceChild(item, itemContainer.lastChild);
   }
   itemContainer.appendChild(item);
   if (localStorage.getItem("items")) {
@@ -42,13 +39,45 @@ const addItem = (e) => {
 };
 inputForm.addEventListener("submit", addItem);
 
+function itemAdd(itemtoAdd) {
+  const item = document.createElement("li");
+  const itemTextContainer = document.createElement("p");
+  const itemText = document.createTextNode(`${itemtoAdd}`);
+  item.className =
+    "w-[48%] bg-white px-2 py-2 rounded-md flex items-center justify-between";
+  const icon = document.createElement("span");
+  icon.className = "material-icons text-red-700 cursor-pointer close";
+  icon.style.fontSize = "1.5rem";
+  const iconText = document.createTextNode("close");
+  icon.appendChild(iconText);
+  itemTextContainer.appendChild(itemText);
+  item.appendChild(itemTextContainer);
+  item.appendChild(icon);
+  return item;
+}
 // text displayed when there's no items
-function noItemsText() {
+function noItemsText(textItem, filterCount = 1) {
   if (itemContainer.children.length === 0) {
     const text = document.createElement("small");
-    const textContent = document.createTextNode("No items...");
+    const textContent = document.createTextNode(textItem);
     text.appendChild(textContent);
     itemContainer.appendChild(text);
+  }
+  if (filterCount == 0) {
+    if (itemContainer.lastChild.className == "filterText") {
+      return;
+    } else {
+      const text = document.createElement("small");
+      text.className = "filterText";
+      const textContent = document.createTextNode(textItem);
+      text.appendChild(textContent);
+      itemContainer.appendChild(text);
+    }
+  } else {
+    if (document.querySelector(".filterText")) {
+      const item = document.querySelector(".filterText");
+      itemContainer.removeChild(item);
+    }
   }
 }
 
@@ -64,7 +93,7 @@ function deleteItem(e) {
       e.target.parentNode.remove();
     }
   }
-  noItemsText();
+  noItemsText("No items...");
 }
 
 itemContainer.addEventListener("click", deleteItem);
@@ -73,73 +102,50 @@ function clearItem() {
   if (confirm("Are you sure? ")) {
     itemContainer.replaceChildren();
     localStorage.setItem("items", "");
-    noItemsText();
+    noItemsText("No items...");
   }
 }
 clearbtn.addEventListener("click", clearItem);
 
 filter.addEventListener("input", (e) => {
-  let filteredItems;
-  if (e.target.value.trim() == "") {
-    e.target.value = "";
-    return;
-  } else {
-    noItemsText();
-    const filteredNodes = JSON.parse(localStorage.getItem("items"));
-    filteredItems = filteredNodes.filter((item) =>
-      item.includes(e.target.value)
-    );
+  let items = document.querySelectorAll("li");
+  if (items.length == 0) {
+    if (e.inputType == "deleteContentBackward") {
+      return;
+    } else {
+      alert("No items to filter");
+      return;
+    }
   }
-  if (filteredItems.length === 0) {
-    console.log(filteredItems);
-    alert("nothing to filter");
-    return;
-  } else {
-    itemContainer.replaceChildren();
-  }
-  filteredItems.forEach((shopItem) => {
-    const item = document.createElement("li");
-    const itemTextContainer = document.createElement("p");
-    const itemText = document.createTextNode(`${shopItem}`);
-    item.className =
-      "w-[48%] bg-white px-2 py-2 rounded-md flex items-center justify-between";
-    const icon = document.createElement("span");
-    icon.className = "material-icons text-red-700 cursor-pointer close";
-    icon.style.fontSize = "1.5rem";
-    const iconText = document.createTextNode("close");
-    icon.appendChild(iconText);
-    itemTextContainer.appendChild(itemText);
-    item.appendChild(itemTextContainer);
-    item.appendChild(icon);
-    itemContainer.appendChild(item);
+  let filterCount = 0;
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent;
+    if (itemName.indexOf(e.target.value) !== -1) {
+      item.style.display = "flex";
+      filterCount += 1;
+    } else {
+      item.style.display = "none";
+    }
   });
+
+  noItemsText("No filtered items...", filterCount);
 });
 
 function loadItems() {
-  if (localStorage.getItem("items") === "") {
-    noItemsText();
-    return;
-  }
-  if (JSON.parse(localStorage.getItem("items")).length > 0) {
+  if (localStorage.getItem("items")) {
     const items = JSON.parse(localStorage.getItem("items"));
     items.forEach((shopItem) => {
-      const item = document.createElement("li");
-      const itemTextContainer = document.createElement("p");
-      const itemText = document.createTextNode(`${shopItem}`);
-      item.className =
-        "w-[48%] bg-white px-2 py-2 rounded-md flex items-center justify-between";
-      const icon = document.createElement("span");
-      icon.className = "material-icons text-red-700 cursor-pointer close";
-      icon.style.fontSize = "1.5rem";
-      const iconText = document.createTextNode("close");
-      icon.appendChild(iconText);
-      itemTextContainer.appendChild(itemText);
-      item.appendChild(itemTextContainer);
-      item.appendChild(icon);
+      const item = itemAdd(shopItem);
       itemContainer.appendChild(item);
     });
+    const textItem = document.createElement("small");
+    const textContent = document.createTextNode("no filtered items...");
+    textItem.appendChild(textContent);
+    textItem.className = "no_filter_text";
+    textItem.style.display = "none";
+    itemContainer.appendChild(textItem);
   } else {
-    noItemsText();
+    noItemsText("No items...");
   }
 }
 window.addEventListener("DOMContentLoaded", loadItems);
